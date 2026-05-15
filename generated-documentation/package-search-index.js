57448a521cfebf5169e1d7f645164b017c362fac
mustibakri2005-ops <mustibakri2005@gmail.com>
2026-05-15 20:56:33 +0300

    Add files via upload

---
+++ generated-documentation/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.masroofy.controller"},{"l":"com.masroofy.model"},{"l":"com.masroofy.repository"},{"l":"com.masroofy.service"},{"l":"com.masroofy.ui"}];updateSearchResults();
